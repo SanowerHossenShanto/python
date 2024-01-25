4627cb0d3845ab6b98bfe1ced5a32b3a0983a854
SanowerHossenShanto <ssarker515@gmail.com>
2024-01-26 02:11:15 +0600

    copy

---
+++ .vscode/Untitled.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    // int x=3;
+    // int y=8;
+    // int sum=x+y;
+    // cout<<sum<<endl;
+    // int div=y/x;
+    // cout<<div;
+    int a,b;
+    cin>>a;
+    cin>>b;
+
+    cout<<a+b;
+    return 0;
+}
